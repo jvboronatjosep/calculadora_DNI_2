d2cd81947daa80a83e9c4bec5925ba141a08ce74
jvboronatjosep <jvboronatdavid@gmail.com>
2025-06-18 19:01:37 +0200

    primer comit

---
+++ src/CalculadoraDNI2.php
@@ -0,0 +1,27 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use CalculadoraDNI2\DNI;
+
+class DNITest extends TestCase
+{
+    public function testObtenerLetra()
+    {
+        $dniSinLetra = 12345678;  // Ejemplo
+        $calc = new DNI($dniSinLetra);
+
+        $letraEsperada = 'Z'; // 12345678 % 23 = 14 => 'Z'
+
+        $this->assertEquals($letraEsperada, $calc->obtenerLetra());
+    }
+
+    public function testDNIConLetraCompleto()
+    {
+        $dniSinLetra = 12345678;
+        $calc = new DNI($dniSinLetra);
+
+        $dniConLetraEsperado = '12345678Z';
+
+        $this->assertEquals($dniConLetraEsperado, $calc->getDNIConLetra());
+    }
+}
